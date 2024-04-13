ad27bfa4f16b246c4308c90c22d0687175a411b7
krava <kravalex2004@gmail.com>
2024-04-13 13:51:26 +0300

    OOP in typescript

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,44 @@
+class Key {
+  private signature = Math.random();
+
+  getSignature():number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key{
+    return this.key;
+  }
+}
+
+abstract class House{
+  protected door: boolean = false;
+  tenants: Array<Person> = [];
+
+  constructor(protected key: Key) {
+    this.key = key
+  }
+
+  comeIn(person: Person): void{
+    if (this.door) this.tenants.push(person);
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House{
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
